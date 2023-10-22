7b1bdbe15047dfdd1128402f76bab76a81085ed4
Isaac <isaacfunes@icloud.com>
2023-10-22 17:02:36 -0500

    Fixed issue. Used ' instead of dash

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,43 +1,23 @@
 "use client";
 
-import Image from "next/image";
-import { MouseEventHandler } from "react";
+import Image from 'next/image'
 
-export interface CustomButtonProps {
-  title: string;
-  containerStyles?: string;
-  textStyles?: string;
-  rightIcon?: string;
-  handleClick?: MouseEventHandler<HTMLButtonElement>;
+import { CustomButtonProps } from '@/types';
+
+const CustomButton = ({ title, 
+    containerStyles, handleClick }: CustomButtonProps) => {
+  return (
+    <button
+        disabled={false}
+        type={'button'}
+        className={`custom-btn ${containerStyles}`}
+        onClick={handleClick}
+    >
+        <span className={`flex-1`}>
+            {title}
+        </span>
+    </button>
+  )
 }
 
-const CustomButton = ({
-  title,
-  containerStyles,
-  textStyles,
-  rightIcon,
-  handleClick,
-}: CustomButtonProps) => (
-  <button
-    disabled={false}
-    type="button"
-    className={`custom-btn ${containerStyles}`}
-    onClick={handleClick}
-  >
-    <span className={`flex-1 ${textStyles}`}>
-      {title}
-    </span>
-    {rightIcon && (
-      <div className="relative w-6 h-6">
-        <Image
-          className="object-contain"
-          src={rightIcon}
-          alt=""
-          fill
-        />
-      </div>
-    )}
-  </button>
-);
-
-export default CustomButton;
+export default CustomButton
