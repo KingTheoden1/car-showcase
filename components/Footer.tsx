44d2001e5e2c51d41c86241dfe586c2fde22ab71
Isaac <isaacfunes@icloud.com>
2023-10-28 23:01:28 -0500

    Added name and copyright symbol at bottom

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,17 @@ const Footer = () => {
         <div className="flex flex-col
         justify-start items-start gap-6">
           <Image src='/logo.svg' alt='logo'
-          width={118} height={18} className='object-contain' />
+          width={118} height={18}
+          className='object-contain' />
+          <p className="text-base
+          text-gray-700">
+            Carhub 2023 <br />
+            All rights reserved &copy;
+          </p>
+        </div>
+
+        <div className='footer__links'>
+          
         </div>
       </div>
     </footer>
