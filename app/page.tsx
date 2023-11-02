8ecb6bc0ccd81cef38e6b43dc194c81eabd8fde0
Isaac <isaacfunes@icloud.com>
2023-11-01 23:51:40 -0500

    Gave ID of Discover to Home function

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image';
+
 import { Hero } from '@/components';
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className='mt-12 padding-x padding-y
+      max-width' id='discover'>
+
+      </div>
     </main>
   )
 }
