774ac75af6accfdd53896e3f4bcee4e5a435218a
Isaac <isaacfunes@icloud.com>
2023-11-13 17:07:39 -0600

    Created CarDetails interface

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { CustomButtonProps } from '@/types';
 
 const CustomButton = ({ title, 
-    containerStyles, handleClick, btnType }: CustomButtonProps) => {
+    containerStyles, handleClick, btnType, textStyles, rightIcon }: CustomButtonProps) => {
   return (
     <button
         disabled={false}
@@ -13,9 +13,19 @@ const CustomButton = ({ title,
         className={`custom-btn ${containerStyles}`}
         onClick={handleClick}
     >
-        <span className={`flex-1`}>
+        <span className={`flex-1 ${textStyles}`}>
             {title}
         </span>
+        {rightIcon && (
+          <div className="relative w-6 h-6">
+            <Image 
+              src={rightIcon}
+              alt='right icon'
+              fill
+              className='object-contain'
+            />
+          </div>
+        )}
     </button>
   )
 }
