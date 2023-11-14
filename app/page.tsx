d1876613b348211ea6dee8aa1511e6a4006b4b00
Isaac <isaacfunes@icloud.com>
2023-11-14 17:55:33 -0600

    exported searchParams function

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
+import { fetchCars } from '@/utils';
+import { HomeProps } from '@/types';
 import Image from 'next/image';
 import { CarCard, CustomFilter, Hero, SearchBar } from '@/components';
-import { fetchCars } from '@/utils';
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+  });
 
-  const isDataEmpty = !Array.isArray(allCars) || allCars.length
-  < 1 || !allCars;
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
     <main className="overflow-hidden">
@@ -37,16 +43,13 @@ export default async function Home() {
               ))}
             </div>
           </section>
-
         ): (
           <div className='home__error-container'>
             <h2 className='text-black text-xl font-bold'>Opps, no results</h2>
             <p>{allCars?.message}</p>
           </div>
-
         )}
-
       </div>
     </main>
-  )
-}
+  );
+};
