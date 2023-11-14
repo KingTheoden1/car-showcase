635af1d72aa5faede53f3fffcd74c5b74f806f57
Isaac <isaacfunes@icloud.com>
2023-11-14 13:13:44 -0600

    Imported FilterProps from @types

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,12 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
     const headers = {
         'X-RapidAPI-Key': '3284f3090bmsh2216203ed9e94c7p163f0fjsn0758e85f5594',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
         headers: headers,
     });
 
